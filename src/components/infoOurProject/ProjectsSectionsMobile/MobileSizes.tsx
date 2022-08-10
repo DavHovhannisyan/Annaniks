b64c9bebba97d3b0bd56b429113c9a1d0c86c7b9
Artur <meliq.a@mail.ru>
2022-08-10 20:59:54 +0400

    fixid styles ourProject Mobile component

--- src/components/infoOurProject/ProjectsSectionsMobile/MobileSizes.tsx
+++ src/components/infoOurProject/ProjectsSectionsMobile/MobileSizes.tsx
@@ -1,8 +1,19 @@
 import React, {FC} from 'react';
 import styles from '../../../styles/OurProjectMobile.module.css'
 import Image from "next/image";
+import Slider from "react-slick";
 
-const MobileSizes:FC = () => {
+
+const settings = {
+    dots: true,
+    dotsClass:styles.dots,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    arrows: false,
+};
+
+const MobileSizes: FC = () => {
     return (
         <>
             <div className={styles.mobile_big_items_container}>
@@ -20,7 +31,26 @@ const MobileSizes:FC = () => {
                 </div>
             </div>
             <div className={styles.mobile_big_items_container_carousel}>
-
+                <Slider {...settings}>
+                    <div style={{ backgroundColor:"#101639",margin:"0 auto"}}>
+                        <div className={styles.mobile_big_items_carousel}>
+                            <Image layout="fill" src='/images/ourProjectImg/phoneImg/without_color.svg'
+                                   alt={'Phone_Image'}/>
+                        </div>
+                    </div>
+                    <div style={{ backgroundColor:"#101639"}}>
+                        <div className={styles.mobile_big_items_carousel}>
+                            <Image layout="fill" src='/images/ourProjectImg/phoneImg/without_color.svg'
+                                   alt={'Phone_Image'}/>
+                        </div>
+                    </div>
+                    <div style={{ backgroundColor:"#101639"}}>
+                        <div className={styles.mobile_big_items_carousel}>
+                            <Image layout="fill" src='/images/ourProjectImg/phoneImg/without_color.svg'
+                                   alt={'Phone_Image'}/>
+                        </div>
+                    </div >
+                </Slider>
             </div>
         </>
     );
